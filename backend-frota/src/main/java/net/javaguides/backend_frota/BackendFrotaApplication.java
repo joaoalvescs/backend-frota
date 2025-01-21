f0a5cca7be777d5cc532a633902c5a208568d4b4
João Alves <joaoalves@Macmini.local>
2025-01-21 03:07:37 -0300

    primeira rota iniciada.

---
+++ backend-frota/src/main/java/net/javaguides/backend_frota/BackendFrotaApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.backend_frota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFrotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFrotaApplication.class, args);
+	}
+
+}
---
+++ backend-frota/src/test/java/net/javaguides/backend_frota/BackendFrotaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.backend_frota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFrotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
